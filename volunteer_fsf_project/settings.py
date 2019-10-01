e668f69bc8830faeef6d3c6fb7befcccf53a8892
John O Sullivan <7268419+Pattern-Projects@users.noreply.github.com>
2019-10-01 18:30:32 +0000

    Update settings

--- volunteer_fsf_project/settings.py
+++ volunteer_fsf_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9ui6^@e@5bi1m0)r0g&8_lfve&1br!r278d60r4ausk1pf8#@b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
