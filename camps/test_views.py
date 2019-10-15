15ee960c83ff050222108920232f6e90bb60bf00
John <pattern.projects.hub@gmail.com>
2019-10-15 19:13:46 +0000

    Improve tests

--- camps/test_views.py
+++ camps/test_views.py
@@ -13,20 +13,27 @@ class TestCampViews(TestCase):
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "camps.html")
 
+
     def test_camp_details(self):
         """
         Test that camp_details creates a Camp object and
         render it with the 'camp.html' template.
-        Or return a 404 if the camp is not found
         """
-        camp = Camp(
-            title="Camp"
-            )
+        camp = Camp(title="Camp")
         camp.save()
 
         page = self.client.get("/camps/{0}/".format(camp.id))
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "camp.html")
+
+
+    def test_camp_details_of_uknown_camp(self):
+        """
+        Test that a 404 is returned if the camp id
+        does not match an existing camp
+        """
+        camp = Camp(title="Camp")
+        camp.save()
 
         page = self.client.get("/camps/{0}/".format(2))
         self.assertEqual(page.status_code, 404)
@@ -34,13 +41,31 @@ class TestCampViews(TestCase):
 
     def test_create_or_edit_a_volunteer_camp(self):
         """
-        test that a volunteer camp is opened with the 'edit_camp.html' template
-        and that /new_camp redirects to /camps/new_camp/
+        test that a volunteer camp is opened with the
+        'edit_camp.html' template
         """
         page = self.client.get("/camps/new_camp/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "edit_camp.html")
 
+
+    def test_for_redirect(self):
+        """
+        Test that browser redirects from '/new_camp'
+        to '/camps/new_camp/
+        """
+
         page = self.client.get("/new_camp")
         self.assertRedirects(page, "/camps/new_camp/", status_code=302, target_status_code=200)
-        # self.assertTemplateUsed(page, "edit_camp.html")
+
+    def test_camp_is_created(self):
+        """
+        Test that a test is created by post method
+        """
+        page = self.client.post("/camps/new_camp/", {"title": "New Camp", "country": "Ireland", "organisation": "VI", "description": "A camp"})
+        camp = get_object_or_404(Camp, pk=1)
+        self.assertEqual("New Camp", camp.title)
+        self.assertEqual("Ireland", camp.country)
+        self.assertEqual("VI", camp.organisation)
+        self.assertEqual("A camp", camp.description)
+
