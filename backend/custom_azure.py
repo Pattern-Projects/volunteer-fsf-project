5e009f8e1bb7728fabf969714c9246793a13f42f
John <pattern.projects.hub@gmail.com>
2019-11-20 12:33:09 +0000

    Azure static file setup

---
+++ backend/custom_azure.py
@@ -0,0 +1,14 @@
+import os
+from storages.backends.azure_storage import AzureStorage
+
+class AzureMediaStorage(AzureStorage):
+    account_name = 'volunteerfsfproject' # Must be replaced by your <storage_account_name>
+    account_key = 'your_key_here' # Must be replaced by your <storage_account_key>
+    azure_container = 'media'
+    expiration_secs = None
+
+class AzureStaticStorage(AzureStorage):
+    account_name = 'volunteerfsfproject' # Must be replaced by your storage_account_name
+    account_key =  os.environ.get('AZURE_ACCESS_KEY') # Must be replaced by your <storage_account_key>
+    azure_container = 'static'
+    expiration_secs = None
--- volunteer_fsf_project/settings.py
+++ volunteer_fsf_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django_forms_bootstrap',
     'authentication',
     'camps',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -144,3 +145,17 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
+
+####### Tested code
+
+DEFAULT_FILE_STORAGE = 'backend.custom_azure.AzureMediaStorage'
+STATICFILES_STORAGE = 'backend.custom_azure.AzureStaticStorage'
+
+STATIC_LOCATION = "static"
+MEDIA_LOCATION = "media"
+
+AZURE_ACCOUNT_NAME = os.environ.get("AZURE_ACCOUNT_NAME")
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
