30b81bf81b4beca99a36620960d12adef54d2b18
John <pattern.projects.hub@gmail.com>
2019-11-24 09:14:36 +0000

    Add more authentication test

--- authentication/test_forms.py
+++ authentication/test_forms.py
@@ -11,3 +11,15 @@ class modelTestCase(TestCase):
         def test_login_missing_name(self):
             form = UserLoginForm({'username':'', 'password':'password'})
             self.assertEqual(form.errors['username'], [u'This field is required.'])
+
+        def test_registration_success(self):
+            form = UserRegistrationForm({'email': 'email@mail.com', 'username':"John", 'password1':'adsjNSID34', 'password2':'adsjNSID34'})
+            self.assertTrue(form.is_valid())
+            
+        def test_registration_missing_name(self):
+            form = UserRegistrationForm({'email': 'email@mail.com', 'username':"", 'password1':'adsjNSID34', 'password2':'adsjNSID34'})
+            self.assertEqual(form.errors['username'], [u'This field is required.'])
+
+        def test_registration_bad_email(self):
+            form = UserRegistrationForm({'email': 'email', 'username':"John", 'password1':'adsjNSID34', 'password2':'adsjNSID34'})
+            self.assertEqual(form.errors['email'], [u'Enter a valid email address.'])
