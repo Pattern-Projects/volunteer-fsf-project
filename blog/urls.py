3a730787721e83a4967f19128c1b7334d5fce896
John <pattern.projects.hub@gmail.com>
2019-12-14 20:32:29 +0000

    Add blog functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
-from .views import get_travel_posts
+from .views import get_travel_posts, post_details, create_or_edit_a_post
 
 urlpatterns = [
     url(r'^$', get_travel_posts, name='get_travel_posts'),
+    url(r'^(?P<pk>\d+)$', post_details, name='post_details'),
+    url(r'new_post/$', create_or_edit_a_post, name='new_post'),
+    url(r'^(?P<pk>\d+)/edit_post$', create_or_edit_a_post, name='edit_post'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import BlogPostForm
+
 # Create your views here.
 def get_travel_posts(request):
     """
@@ -7,3 +9,31 @@ def get_travel_posts(request):
     """
     posts = Post.objects.all().order_by('-published_date')
     return render(request, "posts.html", {'posts': posts})
+
+def post_details(request, pk):
+    """
+    Create a view that returns a single
+    Post object based on the post ID (pk) and
+    render it tot hte 'postdetauils.html' template.
+    Or return a 404 if the post is not found
+    """
+    post = get_object_or_404(Post, pk=pk)
+    post.views += 1
+    post.save()
+    return render(request, "post.html", {'post': post})
+
+def create_or_edit_a_post(request, pk=None):
+    """
+    A view that is used to create
+    or edit a post depending on the Post ID
+    And rendering it to the 'edit_post.html' template
+    """
+    post = get_object_or_404(Post, pk=pk) if pk else None
+    if request.method == "POST":
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post_details, post.pk)
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'edit_post.html', {'form': form})
