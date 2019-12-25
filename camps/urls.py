1304bfbafa712ad1ea6770c4f2b3efffd7f4fd16
John <pattern.projects.hub@gmail.com>
2019-12-25 13:04:44 +0000

    Reorder nav bar

--- camps/urls.py
+++ camps/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from .views import get_camps, camp_details, filter_by_continent, create_or_edit_a_volunteer_camp, archive_camp, get_archived_camps, delete_camp
 
 urlpatterns = [
+    url(r'^$', get_camps, name='camps'),
     url(r'^$', get_camps, name='get_camps'),
     url(r'^(?P<pk>\d+)$', camp_details, name='camp_details'),
     url(r'^filter_by_continent/(?P<continent>\w+)', filter_by_continent, name='filter_by_continent'),
--- volunteer_fsf_project/urls.py
+++ volunteer_fsf_project/urls.py
@@ -8,7 +8,7 @@ from .settings import MEDIA_ROOT
 
 from .views import error404, error500
 from authentication.views import login, registration, logout
-from camps.views import get_camps
+from travel.views import get_travel_post
 import authentication.urls as urls_authentication
 import stories.urls as urls_stories
 import camps.urls as urls_camps
@@ -21,10 +21,10 @@ import travel.urls as urls_travel
 urlpatterns = [
     url(r'^admin', admin.site.urls),
 
-    url(r'^$', get_camps, name="home"),
+    url(r'^$', get_travel_post, name="home"),
     url(r'^authentication/', include(urls_authentication)),
     url(r'^stories/', include(urls_stories), name="stories"),
-    url(r'^camps/', include(urls_camps)),
+    url(r'^camps/', include(urls_camps), name="camps"),
     url(r'^cart/', include(urls_cart)),
     url(r'^checkout/', include(urls_checkout)),
     url(r'^news/', include(urls_news), name="news"),
