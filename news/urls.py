74018e9d7ad2fc7d8e807661a4eef2cdf6c8a297
John <pattern.projects.hub@gmail.com>
2020-01-02 16:07:54 +0000

    Fix News Bug

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,7 @@ from .views import get_posts, post_details, create_or_edit_a_post
 
 urlpatterns = [
     url(r'^$', get_posts, name='news'),
+    url(r'^$', get_posts, name='get_posts'),
     url(r'^(?P<pk>\d+)$', post_details, name='post_details'),
     url(r'new_post/$', create_or_edit_a_post, name='new_post'),
     url(r'^(?P<pk>\d+)/edit_post$', create_or_edit_a_post, name='edit_post'),
